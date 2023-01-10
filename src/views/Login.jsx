3098147aefeb9760672ee5a44665436de3d549c3
Kunniii <nguyenairport56@gmail.com>
2023-01-11 02:23:18 +0700

    looks ok

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -15,12 +15,12 @@ export default function Login() {
   const handleSubmit = (e) => {
     saveToken(userToken);
     e.preventDefault();
-  }
+  };
 
   return (
     <Fragment>
       <div className="text-center w-96 mx-auto">
-        <h1 className="text-center text-4xl font-bold mt-20 pb-10">EDN Auto</h1>
+        <h1 className="text-center text-4xl font-bold mt-52 pb-10">EDN Auto</h1>
         <form onSubmit={handleSubmit}>
           <input
             className="p-2 w-72 mb-2 border rounded-md"
@@ -30,13 +30,33 @@ export default function Login() {
             }}
           />
           <br />
-          <button
-            className="bg-sky-500 border-2 border-sky-600 hover:duration-200 duration-200 hover:bg-transparent hover:border-sky-900 hover:text-sky-900 px-4 py-2 mb-10 mt-3 rounded-lg text-white font-bold hover:animate-pulse"
-          >Login
+          <button className="hover:bg-sky-600 border-2 hover:border-sky-600 hover:duration-200 duration-200 bg-transparent border-sky-600 text-sky-600 px-4 py-2 mb-52 mt-3 rounded-lg hover:text-white font-bold animate-pulse">
+            Login
           </button>
-          <span className="py-10">
+          <span className="">
             <p>Don't know where to find your token?</p>
-            <p>Donwload and install <a className="underline font-bold text-blue-500" href="/EDNTokenGetter.zip" download>this extension</a></p>
+            <p>
+              Donwload and install{" "}
+              <a
+                className="underline font-bold text-blue-500"
+                href="/EDNTokenGetter.zip"
+                download
+              >
+                this extension
+              </a>
+            </p>
+            <p>
+              Then go to{" "}
+              <a
+                className="underline font-bold text-blue-500"
+                href="http://fu.edunext.vn"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                EduNext
+              </a>{" "}
+              to get AccessToken.
+            </p>
           </span>
         </form>
       </div>
